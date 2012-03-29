c223ce9f9f0b51b97353d3b6cd60ffbe3bf9d234
Ji-Hun Cho <jihun268@gmail.com>
2012-03-28 23:06:30 -0700

    add icon and pop-up on maps

--- src/com/cs194/tourme/ShowMapsActivity.java
+++ src/com/cs194/tourme/ShowMapsActivity.java
@@ -1,33 +1,35 @@
 package com.cs194.tourme;
 
+import java.util.List;
+
 import android.content.Context;
-import android.location.Location;
+import android.graphics.drawable.Drawable;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class ShowMapsActivity extends MapActivity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
 		double currLat = 0.0, currLong = 0.0;
 		GeoPoint currentPos;
 		MapController mc;
 		MapView mapView;
 		LocationManager mlocManager;
 		LocationListener mlocListener;
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.maps);
-		
+
 		mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
 
@@ -37,8 +39,8 @@ public class ShowMapsActivity extends MapActivity{
 
 		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 30*10000, 0, mlocListener);
 
-//		Berkeley GeoLocation 37.87309	-122.25921
-		
+		//		Berkeley GeoLocation 37.87309	-122.25921
+
 		try {
 			currLat = mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
 			currLong = mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
@@ -47,14 +49,32 @@ public class ShowMapsActivity extends MapActivity{
 			currLat = 37.87309;
 			currLong = -122.25921;
 		}
-		
+
 		currentPos = new GeoPoint ( (int) (currLat * 1E6) , (int) (currLong * 1E6));
 		mc = mapView.getController();
 		mc.animateTo(currentPos);
-		mc.setZoom(13);
+		mc.setZoom(10);
 		mc.setCenter(currentPos);
 		mapView.invalidate();
-	
+
+
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		// draw star at berkeley, if you click the star then the message pops up
+		// or you can customize 
+		Drawable drawable = this.getResources().getDrawable(R.drawable.star_small);
+		CustomItemizedOverlay itemizedOverlay =
+				new CustomItemizedOverlay(drawable, this);
+
+		GeoPoint point = new GeoPoint(37873090, -122259210);
+		OverlayItem overlayitem =
+				new OverlayItem(point, "Attraction", "Berkeley City");
+
+		itemizedOverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedOverlay);
+
+		MapController mapController = mapView.getController();
+
+		mapController.animateTo(point);
 	}
 
 	@Override
