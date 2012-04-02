b143a420e062a04c6e98f14588b600a794f9d426
hmy <yoo9189@gmail.com>
2012-04-02 13:34:51 -0700

    fixing conflict in DBHandler

--- src/com/cs194/tourme/DatabaseHandler.java
+++ src/com/cs194/tourme/DatabaseHandler.java
@@ -13,6 +13,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 
 import android.util.Log;
@@ -27,20 +30,25 @@ public class DatabaseHandler {
 		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
 
 		try{
-			HttpClient httpclient = new DefaultHttpClient();
+			HttpParams httpParameters = new BasicHttpParams();
+			int timeoutConnection = 2000;
+			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+			int timeoutSocket = 2500;
+			HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+			HttpClient httpclient = new DefaultHttpClient(httpParameters);
 			//"\\s" same as " "
-			
+
 			//caret signs need this....
 			String lessThan = URLEncoder.encode("<", "UTF-8");
 			String greaterThan = URLEncoder.encode(">", "UTF-8");
 			String query = sql.replaceAll("\\s", "%20").replaceAll("<", lessThan).replaceAll(">", greaterThan);
-			
+
 			HttpPost httppost = new HttpPost(PHPSERVERLOCATION + query);
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 			HttpResponse response = httpclient.execute(httppost);
 			HttpEntity entity = response.getEntity();
 			is = entity.getContent();
-			
+
 			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
 			sb = new StringBuilder();
 			sb.append(reader.readLine() + "\n");
@@ -53,14 +61,14 @@ public class DatabaseHandler {
 			}
 			is.close();
 			result=sb.toString();
-			
-			
+
+
 			Log.d("DBHANDLER", sb.toString());
 			return new JSONArray(result);
 		} catch(Exception e){
 			Log.d("DBHANDLER ERROR", "caught Exception e, Most likely Query returned a null");
 			e.printStackTrace();
-			return null;
+			return new JSONArray();
 		}
 	}
 }
