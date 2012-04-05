2baaddfbd7efffbf1c845ef884fbc9141dc89782
hmy <yoo9189@gmail.com>
2012-04-04 20:19:25 -0700

    Changed AndroidManifest.xml
    Now the each activity has no title bars with it

--- gen/com/cs194/tourme/R.java
+++ gen/com/cs194/tourme/R.java
@@ -33,25 +33,26 @@ public final class R {
         public static final int yellow_menu_btn=0x7f020010;
     }
     public static final class id {
-        public static final int ScrollViewForImage=0x7f070004;
-        public static final int ScrollViewForLongText=0x7f070006;
-        public static final int buttonLinkToAttractions=0x7f07000f;
-        public static final int buttonLinkToMap=0x7f070010;
-        public static final int buttonNext=0x7f07000c;
-        public static final int buttonPause=0x7f07000b;
-        public static final int buttonPlay=0x7f07000a;
-        public static final int buttonPrevious=0x7f070009;
-        public static final int buttonShowInstruction=0x7f070011;
-        public static final int buttonTakePicture=0x7f07000d;
+        public static final int ScrollViewForImage=0x7f070005;
+        public static final int ScrollViewForLongText=0x7f070007;
+        public static final int buttonLinkToAttractions=0x7f070010;
+        public static final int buttonLinkToMap=0x7f070011;
+        public static final int buttonNext=0x7f07000d;
+        public static final int buttonPause=0x7f07000c;
+        public static final int buttonPlay=0x7f07000b;
+        public static final int buttonPrevious=0x7f07000a;
+        public static final int buttonShowInstruction=0x7f070012;
+        public static final int buttonTakePicture=0x7f07000e;
         public static final int image=0x7f070002;
-        public static final int imageView1=0x7f070005;
-        public static final int linearLayout=0x7f070008;
+        public static final int imageView1=0x7f070006;
+        public static final int linearLayout=0x7f070009;
         public static final int mapview=0x7f070000;
         public static final int name=0x7f070003;
-        public static final int spinnerChooseLanguage=0x7f070012;
+        public static final int spinnerChooseLanguage=0x7f070013;
         public static final int spinnerChoosePOI=0x7f070001;
-        public static final int textViewAttractionDescription=0x7f070007;
-        public static final int videoView1=0x7f07000e;
+        public static final int textViewAttractionDescription=0x7f070008;
+        public static final int textViewParents=0x7f070004;
+        public static final int videoView1=0x7f07000f;
     }
     public static final class layout {
         public static final int attractionroute=0x7f030000;
