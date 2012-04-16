e094b750f2bb7032ef6491195e7cb19992bf715d
Anastasia <aganchenko@gmail.com>
2012-04-15 20:09:05 -0700

    worked on frontend

--- gen/com/cs194/tourme/R.java
+++ gen/com/cs194/tourme/R.java
@@ -15,22 +15,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int background_simple=0x7f020001;
-        public static final int blue_menu_btn=0x7f020002;
-        public static final int button_next_64=0x7f020003;
-        public static final int button_pause_64=0x7f020004;
-        public static final int button_play_64=0x7f020005;
-        public static final int button_previous_64=0x7f020006;
-        public static final int camera_48=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int logo=0x7f020009;
-        public static final int map_marker=0x7f02000a;
-        public static final int map_marker2=0x7f02000b;
-        public static final int person_marker=0x7f02000c;
-        public static final int star=0x7f02000d;
-        public static final int star_small=0x7f02000e;
-        public static final int wallpaper=0x7f02000f;
-        public static final int yellow_menu_btn=0x7f020010;
+        public static final int background_horiz=0x7f020001;
+        public static final int background_simple=0x7f020002;
+        public static final int background_simple_horiz=0x7f020003;
+        public static final int blue_menu_btn=0x7f020004;
+        public static final int button_next_64=0x7f020005;
+        public static final int button_pause_64=0x7f020006;
+        public static final int button_play_64=0x7f020007;
+        public static final int button_previous_64=0x7f020008;
+        public static final int camera_48=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int logo=0x7f02000b;
+        public static final int map_marker=0x7f02000c;
+        public static final int map_marker2=0x7f02000d;
+        public static final int person_marker=0x7f02000e;
+        public static final int star=0x7f02000f;
+        public static final int star_small=0x7f020010;
+        public static final int wallpaper=0x7f020011;
+        public static final int yellow_menu_btn=0x7f020012;
     }
     public static final class id {
         public static final int LinearLayout01=0x7f070016;
